e992e7ba4774bc14eebc49b7d40c4fc779781629
Tim Chiang <tychiang922@gmail.com>
2022-05-16 17:16:18 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+var fullName = 'Tim Chiang';
+var isCool = Boolean(true);
+var totalPets = 1;
+console.log('value of fullName: ' + fullName);
+console.log('value of isCool: ' + isCool);
+console.log('value of totalPets: ' + totalPets);
+console.log('typeof fullName: ' + typeof fullName);
+console.log('typeof isCool: ' + typeof isCool);
+console.log('typeof totalPets: ' + typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown: ' + unknown);
+console.log('typeof unknown type: ' + typeof unknown);
+
+var never;
+console.log('value of never: ' + never);
+console.log('typeof never: ' + never);
