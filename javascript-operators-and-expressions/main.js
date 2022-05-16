bcc9bfb345da694232e1ba260f35bece7148fc25
Tim Chiang <tychiang922@gmail.com>
2022-05-16 19:50:28 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 5;
+var area = height * width;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 15;
+var payment = 20;
+var change = payment - bill;
+console.log('value of change: ', change);
+console.log('type of change: ', typeof change);
+
+var quizzes = 90;
+var midterm = 86;
+var final = 96;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Tim';
+var lastName = 'Chiang';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 7;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 15;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
