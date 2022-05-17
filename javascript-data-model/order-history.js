dc5bf1708f0a12f896300a6738aedee68ff22530
Tim Chiang <tychiang922@gmail.com>
2022-05-17 18:59:39 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,88 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    productInfo: [
+      {
+        productDetail: {
+          title: 'JavaScript for impatient programmers',
+          author: 'Rauschmayer, Dr. Axel',
+          price: 31.55
+        },
+        returnWindow: 'Sep 7, 2020'
+      }
+    ],
+    deliveryInfo: {
+      orderPlaced: 'August 4, 2020',
+      orderDelivered: 'Aug 8, 2020'
+    },
+    total: 34.00
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    productInfo: [
+      {
+        productDetail: {
+          title: 'The Timeless Way of Building',
+          author: 'Alexander, Christopher',
+          price: 41.33
+        },
+        returnWindow: 'Aug 19, 2020'
+      }
+    ],
+    deliveryInfo: {
+      orderPlaced: 'July 19, 2020',
+      orderDelivered: 'Jul 20, 2020'
+    },
+    total: 44.33
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    productInfo: [
+      {
+        productDetail: {
+          title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+          author: null,
+          price: 15.98
+        },
+        returnWindow: 'Aug 5, 2020'
+      }
+    ],
+    deliveryInfo: {
+      orderPlaced: 'July 4, 2020',
+      orderDelivered: 'Jul 7, 2020'
+    },
+    total: 17.22
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    productInfo: [
+      {
+        productDetail: {
+          title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          author: null,
+          price: 94.95
+        },
+        returnWindow: 'Aug 4, 2020'
+      },
+      {
+        productDetail: {
+          title: 'The Art of Sql',
+          author: 'Faroult, Staphane',
+          price: 33.99
+        },
+        returnWindow: 'Aug 4, 2020'
+      }
+    ],
+    deliveryInfo: {
+      orderPlaced: 'July 3, 2020',
+      orderDelivered: 'Jul 5, 2020'
+    },
+    total: 17.22
+  }
+];
+
+console.log(orderHistory);
