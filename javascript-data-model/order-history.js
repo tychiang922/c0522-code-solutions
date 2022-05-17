dcb57ae933e41565f41af8832dc8605227c86b18
Tim Chiang <tychiang922@gmail.com>
2022-05-17 19:27:18 +0000

    better property names

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,7 +2,7 @@ var orderHistory = [
   {
     orderNumber: '114-3941689-8772232',
     shipTo: 'JS Masher',
-    productInfo: [
+    items: [
       {
         productDetail: {
           title: 'JavaScript for impatient programmers',
@@ -21,7 +21,7 @@ var orderHistory = [
   {
     orderNumber: '113-9984268-1280257',
     shipTo: 'JS Masher',
-    productInfo: [
+    items: [
       {
         productDetail: {
           title: 'The Timeless Way of Building',
@@ -40,7 +40,7 @@ var orderHistory = [
   {
     orderNumber: '114-2875557-9059409',
     shipTo: 'JS Masher',
-    productInfo: [
+    items: [
       {
         productDetail: {
           title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
@@ -59,7 +59,7 @@ var orderHistory = [
   {
     orderNumber: '113-2883177-2648248',
     shipTo: 'JS Masher',
-    productInfo: [
+    items: [
       {
         productDetail: {
           title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
