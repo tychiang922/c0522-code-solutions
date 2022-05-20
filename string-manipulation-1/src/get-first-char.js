a122e5ec5ca820b2da3daca25e9bef3526a184a5
Tim Chiang <tychiang922@gmail.com>
2022-05-20 20:09:31 +0000

    Completed string-manipulation-1

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,8 @@
+/* exported getFirstChar */
+
+/* -return first character of string
+-return string at 0 to function
+*/
+function getFirstChar(string) {
+  return string[0];
+}
---
+++ string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,7 @@
+/* exported getLastChar */
+/* return the last index of a string
+- string[string.length - 1]
+*/
+function getLastChar(string) {
+  return string[string.length - 1];
+}
