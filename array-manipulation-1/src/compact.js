61e8ac4dd35a6b3d5d01b960a16d170f748a55af
Tim Chiang <tychiang922@gmail.com>
2022-05-20 22:16:45 +0000

    Completed array-manipulation-1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,18 @@
 /* exported compact */
+/* remove falsy from array
+-define new result array
+-use loop to cycle through array
+-in loop check if array at index is truthy or falsy
+-if not "false", continue
+-if true, push
+-return result */
+function compact(array) {
+  var result = [];
+  for (var arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+    if (!array[arrayIndex]) {
+      continue;
+    }
+    result.push(array[arrayIndex]);
+  }
+  return result;
+}
