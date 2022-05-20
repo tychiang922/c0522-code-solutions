3ce56a64c3c9bb7d6ab319b4697dee8d2befda31
Tim Chiang <tychiang922@gmail.com>
2022-05-20 22:44:34 +0000

    Completed object-manipulation-1

---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,7 @@
+/* exported setValue */
+
+/* want to add property and value to an object without returning it
+- assign value to the key property of an object */
+function setValue(object, key, value) {
+  object[key] = value;
+}
