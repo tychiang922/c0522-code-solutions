cdb9eb866c48206da5ff26e223ba1655dec54564
Tim Chiang <tychiang922@gmail.com>
2022-05-23 21:36:57 +0000

    Completed dom-events

---
+++ dom-events/buttons.js
@@ -0,0 +1,23 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('event: ', event);
+  console.log('event target: ', event.target);
+}
+var $click = document.querySelector('.click-button');
+$click.addEventListener('click', handleClick);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log('event: ', event);
+  console.log('event target: ', event.target);
+}
+var $hover = document.querySelector('.hover-button');
+$hover.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button hovered');
+  console.log('event: ', event);
+  console.log('event target: ', event.target);
+}
+var $doubleClick = document.querySelector('.double-click-button');
+$doubleClick.addEventListener('dblclick', handleDoubleClick);
