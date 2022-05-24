d4c102ee4df674e02dba8a0942bc789c67ab749d
Tim Chiang <tychiang922@gmail.com>
2022-05-24 19:03:30 +0000

    fixed js

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,15 @@
-var brightnessClick = 0;
+var brightnessClick = true;
 var $circle = document.querySelector('.circle');
 var $body = document.querySelector('body');
 function circleClick(event) {
-  brightnessClick++;
-  if (brightnessClick % 2 === 0) {
+  if (brightnessClick === true) {
     $body.className = 'light-background';
     $circle.className = 'circle light';
+    brightnessClick = false;
   } else {
     $body.className = 'dark-background';
     $circle.className = 'circle dark';
+    brightnessClick = true;
   }
 }
 
