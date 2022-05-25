3a40f5cf18f1c02c9566fca896d2bbdd3c3af375
Tim Chiang <tychiang922@gmail.com>
2022-05-25 17:20:29 +0000

    fixed Event Listener

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,11 +1,11 @@
 function handleFocus(event) {
   console.log('focus has been fired');
-  console.log(event.target.name);
+  console.log('value of target name: ', event.target.name);
 }
 
 function handleBlur(event) {
   console.log('blur has been fired');
-  console.log(event.target.name);
+  console.log('value of target name: ', event.target.name);
 }
 
 function handleInput(event) {
@@ -13,13 +13,18 @@ function handleInput(event) {
   console.log('value of target.value: ', event.target.value);
 }
 
-var $input = document.querySelector('input');
-var $textArea = document.querySelector('textarea');
+var $userName = document.querySelector('#user-name');
+var $userEmail = document.querySelector('#user-email');
+var $userMsg = document.querySelector('#user-message');
 
-$input.addEventListener('focus', handleFocus);
-$input.addEventListener('blur', handleBlur);
-$input.addEventListener('input', handleInput);
+$userName.addEventListener('focus', handleFocus);
+$userName.addEventListener('blur', handleBlur);
+$userName.addEventListener('input', handleInput);
 
-$textArea.addEventListener('focus', handleFocus);
-$textArea.addEventListener('blur', handleBlur);
-$textArea.addEventListener('input', handleInput);
+$userEmail.addEventListener('focus', handleFocus);
+$userEmail.addEventListener('blur', handleBlur);
+$userEmail.addEventListener('input', handleInput);
+
+$userMsg.addEventListener('focus', handleFocus);
+$userMsg.addEventListener('blur', handleBlur);
+$userMsg.addEventListener('input', handleInput);
