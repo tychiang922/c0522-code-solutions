a332ff373c30b9200c48edd720bc86908ff582d2
Tim Chiang <tychiang922@gmail.com>
2022-05-26 07:19:43 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,28 +1,51 @@
 /* exported difference */
 
+/* want to return alue that is different between both input arrays
+1) define result array
+2) define an array to hold words that want to be cut out
+3) define a isCut counter for cutword check
+4) use a for loop of the first array and another for loop of the second array to compare against each other
+  if they match, want to push that value into the cut words array
+5) use a for loop inside of another for loop to check first array against cut words array
+  if they match, set isCut to true and break the cut word loop
+  once cutword loop ends, have a condition where if iscut is still false, push the value into the result array
+6) do step 5 but with second array
+7) return the result */
+
 function difference(first, second) {
   var result = [];
-  var firstArr = [];
-  for (var firstI = 0; firstI < first.length; firstI++) {
-    firstArr.push(first[firstI]);
-  }
-  var secondArr = [];
-  for (var secondI = 0; secondI < second.length; secondI++) {
-    secondArr.push(second[secondI]);
-  }
+  var cutWord = [];
+  var isCut = false;
   for (var firstIndex = 0; firstIndex < first.length; firstIndex++) {
     for (var secondIndex = 0; secondIndex < second.length; secondIndex++) {
       if (first[firstIndex] === second[secondIndex]) {
-        firstArr.splice(firstIndex, 1);
-        secondArr.splice(secondIndex, 1);
+        cutWord.push(first[firstIndex]);
       }
     }
   }
-  for (var i = 0; i < firstArr.length; i++) {
-    result.push(firstArr[i]);
+  for (var firstI = 0; firstI < first.length; firstI++) {
+    isCut = false;
+    for (var cutIndex = 0; cutIndex < cutWord.length; cutIndex++) {
+      if (first[firstI] === cutWord[cutIndex]) {
+        isCut = true;
+        break;
+      }
+    }
+    if (isCut === false) {
+      result.push(first[firstI]);
+    }
   }
-  for (var x = 0; x < secondArr.length; x++) {
-    result.push(secondArr[x]);
+  for (var secondI = 0; secondI < second.length; secondI++) {
+    isCut = false;
+    for (var cutIndex2 = 0; cutIndex2 < cutWord.length; cutIndex2++) {
+      if (second[secondI] === cutWord[cutIndex2]) {
+        isCut = true;
+        break;
+      }
+    }
+    if (isCut === false) {
+      result.push(second[secondI]);
+    }
   }
   return result;
 }
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,38 @@
 /* exported union */
+
+/* want to join first and second array and then apply unique function
+1) define empty array
+2) use for loops to push all of first and second array into the empty array
+3) run unique function
+4) return result */
+
+function union(first, second) {
+  var array = [];
+  for (var fIndex = 0; fIndex < first.length; fIndex++) {
+    array.push(first[fIndex]);
+  }
+  for (var sIndex = 0; sIndex < second.length; sIndex++) {
+    array.push(second[sIndex]);
+  }
+  var sort = [];
+  var result = [];
+  for (var arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+    sort.push(array[arrayIndex]);
+  }
+  sort.sort();
+  var uniqSort = [sort[0]];
+  for (var sortIndex = 1; sortIndex < sort.length; sortIndex++) {
+    if (sort[sortIndex] !== sort[sortIndex - 1]) {
+      uniqSort.push(sort[sortIndex]);
+    }
+  }
+  for (var arrayIndex2 = 0; arrayIndex2 < array.length; arrayIndex2++) {
+    for (var unSortIndex = 0; unSortIndex < uniqSort.length; unSortIndex++) {
+      if (array[arrayIndex2] === uniqSort[unSortIndex]) {
+        uniqSort.splice(unSortIndex, 1);
+        result.push(array[arrayIndex2]);
+      }
+    }
+  }
+  return result;
+}
