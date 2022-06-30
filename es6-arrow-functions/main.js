c17808a979afb11dc178d017cab0489ce90388cf
Tim Chiang <tychiang922@gmail.com>
2022-06-30 17:07:17 +0000

    draft 3

--- es6-arrow-functions/main.js
+++ es6-arrow-functions/main.js
@@ -3,37 +3,40 @@ const $flashImage = document.querySelector('#flash-image');
 const $jokeForm = document.querySelector('#joke-form');
 
 const jokester = {
-  tellJoke: function (setup, punchline) {
+  tellJoke: (setup, punchline) => {
     $jokeForm.classList.add('d-none');
     const $introStatement = this.renderJokePhrase('Hey Flash...');
     this.appendJokePhrase($introStatement);
-    setTimeout(function () {
-      const $jokeSetup = this.renderJokePhrase(setup);
-      this.appendJokePhrase($jokeSetup);
-      setTimeout(function () {
-        const $jokePunchline = this.renderJokePhrase(punchline);
-        this.appendJokePhrase($jokePunchline);
-        flash.laugh();
+    setTimeout(
+      () => {
+        const $jokeSetup = this.renderJokePhrase(setup);
+        this.appendJokePhrase($jokeSetup);
+        setTimeout(
+          () => {
+            const $jokePunchline = this.renderJokePhrase(punchline);
+            this.appendJokePhrase($jokePunchline);
+            flash.laugh();
+          }, 2000);
       }, 2000);
-    }, 2000);
   },
-  renderJokePhrase: function (phrase) {
+  renderJokePhrase: phrase => {
     const $phrase = document.createElement('h4');
     $phrase.textContent = phrase;
     $phrase.className = 'fade-in text-center';
     return $phrase;
   },
-  appendJokePhrase: function ($phrase) {
+  appendJokePhrase: $phrase => {
     $jokeContainer.append($phrase);
   }
 };
 
 const flash = {
   laughingUrl: 'images/flash-laugh.gif',
-  laugh: function () {
-    setTimeout(function () {
-      $flashImage.setAttribute('src', this.laughingUrl);
-    }, 3000);
+  laugh: () => {
+    setTimeout(
+      () => {
+        $flashImage.setAttribute('src', this.laughingUrl);
+      }, 3000);
   }
 };
 
