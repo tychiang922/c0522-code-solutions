2c358beabff2b9f72aa520c02bdccd33fbda7e0f
Tim Chiang <tychiang922@gmail.com>
2022-07-02 00:54:54 +0000

    Completed express-get-json

--- express-get-json/index.js
+++ express-get-json/index.js
@@ -1,4 +1,34 @@
 var express = require('express');
 var app = express();
+
+var grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
 app.listen(3000, () => {
+});
+
+app.get('/api/grades', (req, res) => {
+  const grade = [];
+  for (const key in grades) {
+    grade.push(grades[key]);
+  }
+  res.json(grade);
 });
