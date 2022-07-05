6325b9e9ea05a1cda2d5d13ab4ce2d36e11ac676
Tim Chiang <tychiang922@gmail.com>
2022-07-05 19:55:50 +0000

    Can get single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -13,6 +13,23 @@ fs.readFile('data.json', 'utf-8', (err, txt) => {
     notesArray.push(txtParsed.notes[key]);
   }
   app.get('/api/notes', (req, res) => {
-    res.json(notesArray);
+    res.status(200).send(notesArray);
+  });
+  app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const idString = (req.params.id).toString();
+    if (id <= 0) {
+      const errorMsg = {
+        error: 'id must be a postive integer'
+      };
+      res.status(400).send(errorMsg);
+    } else if (txtParsed.notes[idString] === undefined) {
+      const errorMissingMsg = {
+        error: `cannot find note with id ${id}`
+      };
+      res.status(404).send(errorMissingMsg);
+    }
+    const notesSpecific = txtParsed.notes[id];
+    res.status(200).send(notesSpecific);
   });
 });
