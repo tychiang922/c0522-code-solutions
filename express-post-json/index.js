f6916844713e2d282b69d5f061429a2f9687e7a9
Tim Chiang <tychiang922@gmail.com>
2022-07-06 00:18:25 +0000

    revised express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
-const grades = [];
+const grades = {};
 let nextId = 1;
 app.listen(3000, () => {
 });
 app.get('/api/grades', (req, res) => {
-  res.json(grades);
+  const gradesArray = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
 });
 
 app.use(express.json());
 app.post('/api/grades', (req, res) => {
   req.body.id = nextId;
+  grades.nextId = req.body;
   nextId++;
-  grades.push(req.body);
   res.status(201).send(req.body);
 });
