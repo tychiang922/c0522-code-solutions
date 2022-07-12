57d013cbcb999cd652dd79dbca77d4c34de2f3af
Tim Chiang <tychiang922@gmail.com>
2022-07-12 22:25:44 +0000

    Completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const h1 = React.createElement(
+  'h1',
+  null,
+  'Hello, React!'
+);
+console.log(h1);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(h1);
