7030d8498b0d796d9ce7bff3a52e0e470cbc3e6c
Tim Chiang <tychiang922@gmail.com>
2022-07-13 18:09:50 +0000

    Revised render argument

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -7,4 +7,4 @@ function CustomButton() {
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(CustomButton());
+root.render(<CustomButton />);
