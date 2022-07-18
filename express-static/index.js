a6a1851f1b947f8bd084e8cff4bc5076d6c6e00b
Tim Chiang <tychiang922@gmail.com>
2022-07-18 21:44:07 +0000

    revised express.static argument

--- express-static/index.js
+++ express-static/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const absPath = path.join(__dirname, 'public');
 console.log(absPath);
-const publicStatic = express.static('public');
+const publicStatic = express.static(absPath);
 app.use(publicStatic);
 app.listen(3000, () => {
 });
