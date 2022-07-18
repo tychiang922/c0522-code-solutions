cb4162b45b5241ba1af212f223aef7e1e6edb8d6
Tim Chiang <tychiang922@gmail.com>
2022-07-18 21:42:39 +0000

    draft2

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -4,30 +4,18 @@ export default class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: ''
+      password: '',
+      passwordLength: 0
     };
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
   }
 
   handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  handleMsg() {
-    if (this.state.password.length === 0 || this.state.password.length <= 8) {
-      return (
-      <div className='inline'>
-        <i className="fa-solid fa-xmark"></i>
-        <p>A password is required</p>
-      </div>
-      );
-    } else {
-      return (
-      <div className='inline'>
-        <i className="fa-solid fa-check"></i>
-      </div>
-      );
-    }
+    this.setState({
+      password: event.target.value,
+      passwordLength: event.target.value.length
+    });
+    console.log(this.state);
   }
 
   render() {
@@ -37,7 +25,6 @@ export default class ValidatedInput extends React.Component {
           Password
           <div>
             <input onChange={this.handlePasswordChange} value={this.state.password} type='password' name='password'></input>
-            <this.handleMsg />
           </div>
         </label>
       </form>
