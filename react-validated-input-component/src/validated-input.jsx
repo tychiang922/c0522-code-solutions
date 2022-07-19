eef9a54d8f1b64d0778242b54bc5d7765c6f50e7
Tim Chiang <tychiang922@gmail.com>
2022-07-19 00:12:29 +0000

    draft3

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -15,7 +15,32 @@ export default class ValidatedInput extends React.Component {
       password: event.target.value,
       passwordLength: event.target.value.length
     });
-    console.log(this.state);
+  }
+
+  iconHandler(props) {
+    if (props.charLength <= 8) {
+      return (
+        <i className="fa-solid fa-xmark"></i>
+      );
+    } else {
+      return (
+        <i className="fa-solid fa-check"></i>
+      );
+    }
+  }
+
+  errorMsgHandler(props) {
+    if (props.charLength === 0) {
+      return (
+        <p>Please input a password</p>
+      );
+    } else if (props.charLength <= 8) {
+      return (
+        <p>Password must be longer then 8 characters.</p>
+      );
+    } else {
+      return null;
+    }
   }
 
   render() {
@@ -25,6 +50,8 @@ export default class ValidatedInput extends React.Component {
           Password
           <div>
             <input onChange={this.handlePasswordChange} value={this.state.password} type='password' name='password'></input>
+            <iconHandler charLength={this.state.passwordLength} />
+            <this.errorMsgHandler charLength={this.state.passwordLength} />
           </div>
         </label>
       </form>
