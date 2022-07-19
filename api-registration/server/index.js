115c1ec091f795292f53a3daaf0b68ee844c470e
Tim Chiang <tychiang922@gmail.com>
2022-07-19 22:00:52 +0000

    removed password from return

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -31,7 +31,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const sql = `
     insert into "users" ("username", "hashedPassword")
       values ($1, $2)
-      returning *;
+      returning "username", "userId", "createdAt";
     `;
       const params = [username, hashedPassword];
       db.query(sql, params)
